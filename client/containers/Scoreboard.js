784cb282b90e035575de331bcb24aad9d88379f4
nienkebos <nienkebos@live.com>
2016-09-08 09:40:24 +0200

    scoreboard and secondroll-action

---
+++ client/containers/Scoreboard.js
@@ -0,0 +1,18 @@
+import React, { Component, PropTypes } from 'react'
+
+import newScore from '..actions/new-score'
+
+
+class Scoreboard extends Component {
+  render() {
+    // TODO: const { score } and action score
+    return(
+      <div>
+        <h4>Scoreboard</h4>
+
+      </div>
+    )
+  }
+}
+
+export default Scoreboard
