028188b71dd2db28f1714b4f393b000870031ac9
nienkebos <nienkebos@live.com>
2016-09-08 11:20:07 +0200

    little mergeconflict

--- client/actions/second-roll.js
+++ client/actions/second-roll.js
@@ -1,15 +1,42 @@
-
 import diceRolled from './dice-rolled'
 import newScore from './new-score'
 
 export default function secondRoll(game) {
 
 
+  function turn(x) {
+    newTurn = x.map(function(nr){
+      if (nr == 1 || nr == 5) {
+      return nr
+      } else {
+      return Math.floor((Math.random()*6)+1)
+      }
+    });
+    return newTurn
+  }
+
+  function count(turn,num) {
+    var count = 0;
+    for(var i = 0; i < turn.length; ++i){
+        if(turn[i] == num)
+            count++;
+    }
+    return count;
+  }
+
+  function checkDead(old,nw) {
+    if (count(old,5) == count(nw,5) && count(old,1) == count(nw,1)) {
+      return true
+    } else {
+      return false
+    }
+  }
+
 
   return dispatch => {
 
     dispatch(diceRolled(eyes))
-    dispatch(newScore(score))
+    dispatch(newScore(score, eyes, turn))
 
   }
 }
