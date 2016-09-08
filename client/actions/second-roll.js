3c3d9613934f35c3de8333bbd1a05a92c086eb7e
nienkebos <nienkebos@live.com>
2016-09-08 12:43:38 +0200

    rollagain, unchange 1 and 5

--- client/actions/second-roll.js
+++ client/actions/second-roll.js
@@ -1,42 +1,45 @@
 import diceRolled from './dice-rolled'
 import newScore from './new-score'
 
-export default function secondRoll(game) {
-
-
-  function turn(x) {
-    newTurn = x.map(function(nr){
+export default function secondRoll(player) {
+console.log(player.eyes);
+  const newEyes = player.eyes.map(function(nr){
       if (nr == 1 || nr == 5) {
       return nr
       } else {
       return Math.floor((Math.random()*6)+1)
       }
-    });
-    return newTurn
-  }
+    })
 
-  function count(turn,num) {
-    var count = 0;
-    for(var i = 0; i < turn.length; ++i){
-        if(turn[i] == num)
-            count++;
-    }
-    return count;
-  }
+    const score = newEyes.map(function(no){
+      if (no == 1){ return 100}
+      else if (no == 5){ return 50}
+      else { return 0};
+    }).reduce((x, y) => x + y, 0)
 
-  function checkDead(old,nw) {
-    if (count(old,5) == count(nw,5) && count(old,1) == count(nw,1)) {
-      return true
-    } else {
-      return false
-    }
-  }
+
+  // function count(turn,num) {
+  //   var count = 0;
+  //   for(var i = 0; i < turn.length; ++i){
+  //       if(turn[i] == num)
+  //           count++;
+  //   }
+  //   return count;
+  // }
+  //
+  // function checkDead(old,nw) {
+  //   if (count(old,5) == count(nw,5) && count(old,1) == count(nw,1)) {
+  //     return true
+  //   } else {
+  //     return false
+  //   }
+  // }
 
 
   return dispatch => {
 
-    dispatch(diceRolled(eyes))
-    dispatch(newScore(score, eyes, turn))
+    dispatch(diceRolled(newEyes))
+    dispatch(newScore(score, newEyes))
 
   }
 }
